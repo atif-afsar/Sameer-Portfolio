2c63ade7dc935476daffc35b8effef0910f19a61
Atif Afsar <atifafsar70@gmail.com>
2026-07-03 17:26:02 +0530

    change the experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,2 +1,2 @@
 /** Career start — live counter ticks from this moment (local time). */
-export const CAREER_START = new Date(2020, 2, 15, 9, 0, 0);
+export const CAREER_START = new Date(2024, 3, 1, 9, 0, 0);
